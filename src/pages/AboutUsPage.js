4ee54abb75a3ca2b08daffcf99fb47270116fdfc
nghe4 <nghe4@oocl.com>
2025-09-15 16:41:01 +0800

    feat: add description in about us page

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,3 +1,11 @@
 export const AboutUsPage = () => {
-  return <h1>About Us</h1>;
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>
+        This is a simple todo application built with React, React Router for
+        users to manage their tasks efficiently.
+      </p>
+    </>
+  );
 };

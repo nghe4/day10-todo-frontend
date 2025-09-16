1cadab5e23ba2ea9cddbc225f1d71f2ba7b59023
nghe4 <nghe4@oocl.com>
2025-09-16 17:43:16 +0800

    feat: update API baseURL from mock to springboot localhost endpoint

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { message } from "antd";
 
 const api = axios.create({
-  baseURL: "https://68c7ac8e5d8d9f5147328736.mockapi.io/",
+  baseURL: "http://localhost:8080/",
   headers: { "Content-Type": "application/json" },
   timeout: 1000,
 });
